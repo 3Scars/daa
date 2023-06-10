b0d149366ae29ea5dccfa4f2145fba8a955b0171
3Scars <bernardussurjana@gmail.com>
2023-06-10 09:17:36 +0700

    Pertemuan8

---
+++ Pertemuan8/pert8.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,n;
+    int UTS[5] = {75,88,100,98,50};
+    for(i=0; i<=4; i++){
+        cout<<"Angka ke-"<<i+1<<" = "<<UTS[i]<<endl;}
+        return 0;
+}
